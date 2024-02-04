15dc2b86e44ea93f7ca6847f1864dc9fde22ee88
Muteeb Akram <muteebakram@gmail.com>
2024-02-04 14:20:44 -0700

    Separate pages with web component

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 class MyHeader extends HTMLElement {
+  static observedAttributes = ["title"];
+
+  constructor() {
+    super();
+    this.title = "Muteeb Akram";
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue !== newValue) this.title = newValue;
+  }
   connectedCallback() {
     this.innerHTML = `
     <head>
@@ -7,20 +17,21 @@ class MyHeader extends HTMLElement {
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="icon" type="image/x-icon" href="./assest/favicon.ico">
         <link rel="stylesheet" href="./style.css">
-        <title>Muteeb Akram</title>
+        <title>Muteeb Akram: ${this.title}</title>
     </head>
 
     <header>
         <h1 class="name">Muteeb Akram</h1>
         <hr />
         <div class="row">
-            <p class="row-block"><a href="./index.html">Home</a></p>
-            <p class="row-block"><a href="./experience.html">Experience</a></p>
-            <p class="row-block"><a href="./education.html">Education</a></p>
-            <p class="row-block"><a href="./projects.html">Projects</a></p>
-            <p class="row-block"><a href="./publications.html">Publications</a></p>
-            <p class="row-block"><a href="./contact.html">Contact</a></p>
+            <p><a href="./index.html">Home</a></p>
+            <p><a href="./experience.html">Experience</a></p>
+            <p><a href="./education.html">Education</a></p>
+            <p><a href="./projects.html">Projects</a></p>
+            <p><a href="./publications.html">Publications</a></p>
+            <p><a href="./contact.html">Contact</a></p>
         </div>
+        <h2>${this.title}</h2>
     </header>`;
   }
 }
