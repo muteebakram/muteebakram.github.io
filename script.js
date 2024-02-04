976d66102e6e353842bd49a3521ff0d8834245f9
Muteeb Akram <muteebakram@gmail.com>
2024-02-04 15:59:46 -0700

    Mobile view, link visited remove, add publications

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ class MyHeader extends HTMLElement {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="icon" type="image/x-icon" href="./assest/favicon.ico">
+        <link rel="icon" type="image/x-icon" href="favicon.ico">
         <link rel="stylesheet" href="./style.css">
         <title>Muteeb Akram: ${this.title}</title>
     </head>
@@ -24,12 +24,12 @@ class MyHeader extends HTMLElement {
         <h1 class="name">Muteeb Akram</h1>
         <hr />
         <div class="row">
-            <p><a href="./index.html">Home</a></p>
-            <p><a href="./experience.html">Experience</a></p>
-            <p><a href="./education.html">Education</a></p>
-            <p><a href="./projects.html">Projects</a></p>
-            <p><a href="./publications.html">Publications</a></p>
-            <p><a href="./contact.html">Contact</a></p>
+            <p title="Home"><a href="./index.html">Home</a></p>
+            <p title="Experience"><a href="./experience.html">Experience</a></p>
+            <p title="Education"><a href="./education.html">Education</a></p>
+            <p title="Projects"><a href="./projects.html">Projects</a></p>
+            <p title="Publications"><a href="./publications.html">Publications</a></p>
+            <p title="Contact"><a href="./contact.html">Contact</a></p>
         </div>
         <h2>${this.title}</h2>
     </header>`;
