e7acb26797acbf1fd9a09e5df9bbb8d25a2c7919
Muteeb Akram <muteebakram@gmail.com>
2024-02-18 00:14:03 -0700

    Update last updated

--- script.js
+++ script.js
@@ -361,35 +361,35 @@ class MyHeader extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-    <head>
+      <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="icon" type="image/x-icon" href="favicon.ico">
         <link rel="stylesheet" href="./style.css">
         <title>Muteeb Akram: ${this.title}</title>
-    </head>
+      </head>
 
-    <header>
+      <header>
         <h1 class="name">Muteeb Akram</h1>
         <hr />
         <div class="row">
-            <p title="Home"><a href="./index.html">Home</a></p>
-            <p title="Experience"><a href="./experience.html">Experience</a></p>
-            <p title="Education"><a href="./education.html">Education</a></p>
-            <p title="Projects"><a href="./projects.html">Projects</a></p>
-            <p title="Publications"><a href="./publications.html">Publications</a></p>
-            <p title="Contact"><a href="./contact.html">Contact</a></p>
+          <p title="Home"><a href="./index.html">Home</a></p>
+          <p title="Experience"><a href="./experience.html">Experience</a></p>
+          <p title="Education"><a href="./education.html">Education</a></p>
+          <p title="Projects"><a href="./projects.html">Projects</a></p>
+          <p title="Publications"><a href="./publications.html">Publications</a></p>
+          <p title="Contact"><a href="./contact.html">Contact</a></p>
         </div>
         <h2>${this.title}</h2>
-    </header>`;
+      </header>`;
   }
 }
 
 class MyFooter extends HTMLElement {
   constructor() {
     super();
-    this.lastUpdated = "04 Feb 2024";
+    this.lastUpdated = "18 Feb 2024";
   }
 
   connectedCallback() {
